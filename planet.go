bc839c63deaa02a6079b146d14fb2c598d608de4
Juan Cespedes <juan.cespedes@imdea.org>
2019-01-09 12:09:23 +0100

    auto (@loopy)

--- planet.go
+++ planet.go
@@ -12,9 +12,11 @@ import (
 	"github.com/glacjay/goini"
 )
 
-var general struct {
+var global struct {
 	max_posts_per_author int
 	max_posts_per_page   int
+	template             string
+	output               string
 }
 
 func keys_from_map(in map[string]string) []string {
@@ -45,8 +47,10 @@ func main() {
 		log.Println(err)
 		return
 	}
-	general.max_posts_per_author, _ = strconv.Atoi(config["_general"]["max_posts_per_author"])
-	general.max_posts_per_page, _ = strconv.Atoi(config["_general"]["max_posts_per_page"])
+	global.max_posts_per_author, _ = strconv.Atoi(config["_global"]["max_posts_per_author"])
+	global.max_posts_per_page, _ = strconv.Atoi(config["_global"]["max_posts_per_page"])
+	global.template = config["_global"]["template"]
+	global.output = config["_global"]["output"]
 	x := 0
 	for name, content := range config {
 		x++
@@ -59,7 +63,7 @@ func main() {
 			log.Printf("[%s] %s (%d items)", name, feed.Title, len(feed.Items))
 			i := 0
 			for _, item := range feed.Items {
-				if i >= general.max_posts_per_author {
+				if i >= global.max_posts_per_author {
 					break
 				}
 				entries = append(entries, entry{origin:name, published:*item.PublishedParsed, title:item.Title})
@@ -70,30 +74,30 @@ func main() {
 //		log.Println("[" + name + "] " + fmt.Sprint(keys_from_map(content)))
 	}
 	sort.Slice(entries, func(i, j int) bool { return entries[j].published.Before(entries[i].published) })
-	if len(entries) > general.max_posts_per_page {
-		entries = entries[:general.max_posts_per_page]
+	if len(entries) > global.max_posts_per_page {
+		entries = entries[:global.max_posts_per_page]
 	}
 	for _, entry := range entries {
 		log.Println(entry)
 	}
 	vars["posts"] = make([]map[string]string, 0)
 	for x, y := range config {
-		if x != "_general" {
+		if x != "_global" {
 			vars["posts"] = append(vars["posts"].([]map[string]string), map[string]string(y))
 		}
 	}
 //	vars = config
 //	vars["items"] = config
-//	delete(vars["items"], "_general")
-	for x, y := range config["_general"] {
+//	delete(vars["items"], "_global")
+	for x, y := range config["_global"] {
 		vars[x] = y
 	}
-	t, err := template.ParseFiles("template.html")
+	t, err := template.ParseFiles(global.template)
 	if err != nil {
 		log.Print(err)
 		return
 	}
-	f, err := os.Create("output.html")
+	f, err := os.Create(global.output)
 	if err != nil {
 		log.Println("create file: ", err)
 		return
